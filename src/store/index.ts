e965cb04c22e7f47de81941bf70044694f2821c0
Irin <iraantonova1998@list.ru>
2022-08-25 11:03:04 +0300

    add slice in store reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthorizationSlice from "./auth/slice";
+import UsersSlice from "./users/slice";
 
 export const store = configureStore({
   reducer: {
     auth: AuthorizationSlice,
+    users: UsersSlice,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
