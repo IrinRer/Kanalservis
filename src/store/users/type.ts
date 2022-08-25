97667140001b103612e57502a2219f0b60870d68
Irin <iraantonova1998@list.ru>
2022-08-25 10:40:41 +0300

    started doing store for users & type

---
+++ src/store/users/type.ts
@@ -0,0 +1,10 @@
+import { AxiosError } from "axios";
+export const USERS_SLICE_ALIAS = "users";
+
+export interface IUsersReducer {
+  id: string;
+  name: string;
+  company: string;
+  loading: boolean;
+  error: AxiosError | null;
+}
